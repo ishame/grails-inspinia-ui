57bd420abfa1c07431c0599d099fd8444a6b09b2
Shame <truongdv90813@gmail.com>
2018-01-22 11:21:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# grails-inspinia-ui
+Admin Bootstrap Template Inspinia On Grails
---
+++ grails-app/init/grails/inspinia/ui/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.inspinia.ui
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
