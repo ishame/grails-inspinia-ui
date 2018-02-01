046d736236d1ca60eea9c8114953d81d42346ec7
Shame <truongdv90813@gmail.com>
2018-02-01 14:08:46 +0700

    update #Configuration

--- grails-app/assets/javascripts/inspinia.js
+++ grails-app/assets/javascripts/inspinia.js
@@ -96,11 +96,11 @@ $(document).ready(function () {
     });
 
     // Append config box / Only for demo purpose
-    // Uncomment on server mode to enable XHR calls
-    //$.get("skin-config.html", function (data) {
-    //    if (!$('body').hasClass('no-skin-config'))
-    //        $('body').append(data);
-    //});
+    // Uncomment/Comment on server mode to enable/disable XHR calls
+    $.get("skin-config", function (data) {
+       if (!$('body').hasClass('no-skin-config'))
+           $('body').append(data);
+    });
 
     // Minimalize menu
     $('.navbar-minimalize').on('click', function (event) {
